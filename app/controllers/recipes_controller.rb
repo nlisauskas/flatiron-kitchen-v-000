ef0246a0b9c2d45b6b8f97677c3f9cdf633a0899
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-27 19:27:49 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,17 @@
 class RecipesController < ApplicationController
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
   end
   
 
