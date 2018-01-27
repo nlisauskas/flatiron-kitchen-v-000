4c90167722ffa8281280376c737bde8d0ff81bda
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-27 19:36:21 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,7 +17,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :ingredient_ids: [])
+    params.require(:recipe).permit(:name, ingredient_ids: [])
   end
 
 
